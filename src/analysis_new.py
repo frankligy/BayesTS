b802f50637503ea12a15fe772a7d1a0e7debe0b7
Guangyuan(Frank) Li <frankli296@yahoo.com>
2023-03-02 16:10:21 -0500

    Update analysis_new.py

--- src/analysis_new.py
+++ src/analysis_new.py
@@ -36,20 +36,39 @@ mpl.rcParams['font.family'] = 'Arial'
 
 
 '''
+# task 1: ENSG00000178952:I8.1_28843525-E9.1 TCGA-EE-A3AG-06A-31R-A18S-07 chr16:28843148-28843525 (1500,1500)
+# task 2: ENSG00000000419:E6.1-E8.2 TCGA-EB-A5VU-01A-21R-A32P-07 chr20:50940933-50942031 (1500,1500)
+'''
+
+'''
 inspect those candidates for junction validity and tumor specificity
 '''
+
+# ts = pd.read_csv('../dev/full_results_XY.txt',sep='\t',index_col=0)
+# result = pd.read_csv('/data/salomonis2/LabFiles/Frank-Li/neoantigen/revision/TCGA_melanoma/result_new/frequency_stage0_verbosity1_uid_gene_symbol_coord_mean_mle.txt',sep='\t',index_col=0)
+# count = snaf.remove_trailing_coord('../bayesian/counts.TCGA-SKCM.txt')
+# count_max = pd.Series(index=count.index,data=count.values.max(axis=1)).to_dict()
+# count_n = pd.Series(index=count.index,data=np.count_nonzero(count.values,axis=1)).to_dict()
+# s2g = pd.Series(index=[item.split(',')[0] for item in result.index],data=result['symbol'].values)
+# s2c = pd.Series(index=[item.split(',')[0] for item in result.index],data=result['coord'].values)
+# subset = ts.loc[(ts['mean_sigma']<0.87) & (ts['mean_sigma']>0.86),:]
+# subset['max'] = subset.index.map(count_max).values
+# subset['n'] = subset.index.map(count_n).values
+# subset['gene'] = subset.index.map(s2g).values
+# subset['coord'] = subset.index.map(s2c).values
+# subset.to_csv('area0.86_0.87.txt',sep='\t')
+
+
+uid = 'ENSG00000000419:E6.1-E8.2'
+# count = snaf.remove_trailing_coord('../bayesian/counts.TCGA-SKCM.txt')
+# count.loc[uid,:].to_csv('each_check.txt',sep='\t')
+# sys.exit('stop')
+
+
 control_bam_path_list = ['/data/salomonis-archive/BAMs/NCI-R01/TCGA/TCGA-BREAST-CANCER/TCGA-files-Ens91/bams/TCGA-BH-A208-11A-51R-A157-07.bam',
-                 '/data/salomonis-archive/BAMs/NCI-R01/TCGA/TCGA-Kidney/KIRC/TCGA-CJ-5680-11A-01R-1541-07.bam',
-                 '/data/salomonis-archive/BAMs/NCI-R01/TCGA/TCGA-STAD/TCGA-BR-6453-11A-01R-1802-13.bam',
-                 '/data/salomonis-archive/BAMs/NCI-R01/TCGA/TCGA-ESCA/TCGA-L5-A4OG-11A-12R-A260-31.bam',
-                 '/data/salomonis-archive/BAMs/NCI-R01/TCGA/TCGA-THCA/TCGA-EL-A3ZP-11A.bam',
-                 '/data/salomonis-archive/BAMs/NCI-R01/TCGA/TCGA-BREAST-CANCER/TCGA-files-Ens91/bams/TCGA-BH-A0H7-11A-13R-A089-07.bam']
+                 '/data/salomonis-archive/BAMs/NCI-R01/TCGA/TCGA-Kidney/KIRC/TCGA-CJ-5680-11A-01R-1541-07.bam']
 control_bai_path_list = ['/data/salomonis-archive/BAMs/NCI-R01/TCGA/TCGA-BREAST-CANCER/TCGA-files-Ens91/bams/TCGA-BH-A208-11A-51R-A157-07.bam.bai',
-                 '/data/salomonis-archive/BAMs/NCI-R01/TCGA/TCGA-Kidney/KIRC/TCGA-CJ-5680-11A-01R-1541-07.bam.bai',
-                 '/data/salomonis-archive/BAMs/NCI-R01/TCGA/TCGA-STAD/TCGA-BR-6453-11A-01R-1802-13.bam.bai',
-                 '/data/salomonis-archive/BAMs/NCI-R01/TCGA/TCGA-ESCA/TCGA-L5-A4OG-11A-12R-A260-31.bam.bai',
-                 '/data/salomonis-archive/BAMs/NCI-R01/TCGA/TCGA-THCA/TCGA-EL-A3ZP-11A.bam.bai',
-                 '/data/salomonis-archive/BAMs/NCI-R01/TCGA/TCGA-BREAST-CANCER/TCGA-files-Ens91/bams/TCGA-BH-A0H7-11A-13R-A089-07.bam.bai']
+                 '/data/salomonis-archive/BAMs/NCI-R01/TCGA/TCGA-Kidney/KIRC/TCGA-CJ-5680-11A-01R-1541-07.bam.bai']
 
 def unit_run(uid,base_sample,region,task_name):
     uid = uid
@@ -72,10 +91,10 @@ def flank_chrom(chrom,offset):
     assemble = '{}:{}-{}'.format(sec1,new_sec2,new_sec3)
     return assemble
 
-# unit_run('ENSG00000001461:E6.1-E7.1_24445189','TCGA-EE-A2GD-06A-11R-A18T-07',flank_chrom('chr1:24442226-24445189',(1000,1000)),'task1')
-unit_run('ENSG00000149582:E6.2-E7.1','TCGA-EE-A2GD-06A-11R-A18T-07',flank_chrom('chr11:118534129-118534266',(100,100)),'task2')
 
-
-
+base_sample = 'TCGA-EB-A5VU-01A-21R-A32P-07'
+coord = 'chr20:50940933-50942031'
+region = flank_chrom(coord,(1500,1500))
+unit_run(uid,base_sample,region,'task2')
 
 
